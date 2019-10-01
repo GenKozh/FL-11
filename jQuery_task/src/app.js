e9cf6ae4ac39680c5eed74709ad383ded214f059
GenKozh <gen.kozh@gmail.com>
2019-10-01 06:19:47 +0300

    jQuery task added

--- jQuery_task/src/app.js
+++ jQuery_task/src/app.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       if (/^\d+$/.test(oldValue)) {
         newValue = '+' + oldValue;
         inputElem.val(newValue);
+        this.submit();
       }
-      this.submit();
   });
 }); 
